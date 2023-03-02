f88b6413a0fecade7981a885aaf5c62979f61e4f
Joselu Conde <joseluisconde@protonmail.com>
2023-03-02 15:35:25 +0100

    Update the database with the usersControler

--- src/controllers/usersControllers.mjs
+++ src/controllers/usersControllers.mjs
@@ -1,6 +1,14 @@
 import { users } from "../models/usersModels.mjs";
+import { db } from "../models/db.mjs";
 
 export function postUserController (request, response) {
-    users.push(request.body);
+    const {name, password} = request.body;
+    const sql = `
+    INSERT INTO users ( name, password )
+    values ( '${name}' , '${password}' )
+    `;
+    db.run(sql);
+    
+    //users.push(request.body);
     response.sendStatus(201)
 }
